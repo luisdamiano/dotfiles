a5e05636b2c3ec2c40f4416adc7e3bdbc9c0a998
damianoluis <damiano.luis@gmail.com>
2022-01-13 17:33:59 -0600

    Add org html export fix

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -313,6 +313,23 @@ https://github.com/magit/magit/issues/460 (@cpitclaudel)."
 ;; support code block syntax highlighting in html files
 (use-package htmlize)
 
+;; fix wrong source block background
+;; https://emacs.stackexchange.com/q/3374
+(defun my/org-inline-css-hook (exporter)
+  "Insert custom inline css to automatically set the
+background of code to whatever theme I'm using's background"
+  (when (eq exporter 'html)
+    (let* ((my-pre-bg (face-background 'default))
+           (my-pre-fg (face-foreground 'default)))
+      (setq
+       org-html-head-extra
+       (concat
+        org-html-head-extra
+        (format "<style type=\"text/css\">\n pre.src {background-color: %s; color: %s;}</style>\n"
+                my-pre-bg my-pre-fg))))))
+
+(add-hook 'org-export-before-processing-hook 'my/org-inline-css-hook)
+
 ;; Markdown settings------------------------------------------------------------
 ;;
 
