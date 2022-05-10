83b518fbb8cf929ca855d9462d9774d8eb2af26d
damianoluis <damiano.luis@gmail.com>
2022-05-10 07:30:17 -0500

    Daily commit

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -648,7 +648,7 @@ background of code to whatever theme I'm using's background"
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight normal :height 90 :width normal))))
+ '(default ((t (:family "Roboto Mono" :foundry "PfEd" :slant normal :weight normal :height 90 :width normal))))
  '(font-latex-math-face ((t (:foreground "yellow"))))
  '(font-latex-script-char-face ((t (:foreground "olive drab"))))
  '(font-latex-sectioning-5-face ((t (:foreground "#ff7983"))))
